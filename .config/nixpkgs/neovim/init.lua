62c9f758a3235c909fc46f89004266d2ff218262
Jason Peacock <jason@jasonpeacock.com>
2022-12-18 16:32:50 -0800

    feat(neovim): tune autocomplete/snippet behavior

--- .config/nixpkgs/neovim/init.lua
+++ .config/nixpkgs/neovim/init.lua
@@ -152,14 +152,16 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- Setup Completion
+local luasnip = require("luasnip")
+-- Load VSCode-like snippets, e.g. from `friendly-snippets`
+require("luasnip.loaders.from_vscode").lazy_load()
+
 -- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
 local has_words_before = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-local luasnip = require("luasnip")
 local cmp = require("cmp")
 cmp.setup({
     preselect = cmp.PreselectMode.None,
@@ -206,14 +208,13 @@ cmp.setup({
             end
         end, { "i", "s" }),
     },
-
-    -- Installed sources
     sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "path" },
         { name = "buffer" },
     },
+    completion = { keyword_length = 3 },
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
@@ -340,6 +341,27 @@ require('rust-tools').setup({
     },
 })
 
+-- Rust
+-- https://github.com/numToStr/dotfiles/blob/f972b9ebfd742daac8f2dc5ea6c19681241bd798/neovim/.config/nvim/lua/numToStr/plugins/lsp/servers.lua#L63-L85
+-- require("lspconfig")["rust_analyzer"].setup {
+--     on_attach = on_attach,
+--     capabilities = capabilities,
+--     settings = {
+--         ["rust_analyzer"] = {
+--             cargo = {allFeatures = false},
+--             checkOnSave = {allFeatures = false, command = 'clippy'},
+--             procMacro = {
+--                 enable = true,
+--                 ignored = {
+--                     ['async-trait'] = {'async_trait'},
+--                     ['napi-derive'] = {'napi'},
+--                     ['async-recursion'] = {'async_recursion'}
+--                 }
+--             }
+--         }
+--     }
+-- }
+
 -- Lua
 require('lspconfig')['sumneko_lua'].setup {
     capabilities = capabilities,
@@ -411,27 +433,6 @@ require('lspconfig')['pylsp'].setup {
         },
     },
 }
-
--- Rust
--- https://github.com/numToStr/dotfiles/blob/f972b9ebfd742daac8f2dc5ea6c19681241bd798/neovim/.config/nvim/lua/numToStr/plugins/lsp/servers.lua#L63-L85
--- require("lspconfig")["rust_analyzer"].setup {
---     on_attach = on_attach,
---     capabilities = capabilities,
---     settings = {
---         ["rust_analyzer"] = {
---             cargo = {allFeatures = false},
---             checkOnSave = {allFeatures = false, command = 'clippy'},
---             procMacro = {
---                 enable = true,
---                 ignored = {
---                     ['async-trait'] = {'async_trait'},
---                     ['napi-derive'] = {'napi'},
---                     ['async-recursion'] = {'async_recursion'}
---                 }
---             }
---         }
---     }
--- }
 
 -- nvim-treesitter
 -- https://github.com/nvim-treesitter/nvim-treesitter
@@ -588,71 +589,4 @@ require('nvim-treesitter.configs').setup {
 --             diagnostics_format = diagnostics_msg
 --         }) -- Dockerfile
 --     }
--- })
-
--- luasnip
--- local luasnip = require("luasnip")
--- require("luasnip.loaders.from_vscode").lazy_load() -- Load VSCode-like snippets, e.g. from `friendly-snippets`
-
--- -- From: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
--- local has_words_before = function()
---     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---     return col ~= 0 and
---                vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col,
---                                                                           col)
---                    :match("%s") == nil
--- end
-
--- nvim-cmp
--- vim.opt.completeopt = {"menu", "menuone", "noselect"}
--- local cmp = require("cmp")
--- cmp.setup({
---     snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
---     window = {
---         completion = cmp.config.window.bordered(),
---         documentation = cmp.config.window.bordered()
---     },
---     mapping = cmp.mapping.preset.insert({
---         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
---         ["<C-f>"] = cmp.mapping.scroll_docs(4),
---         ["<C-Space>"] = cmp.mapping.complete(),
---         ["<C-e>"] = cmp.mapping.abort(),
---         -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---         ["<CR>"] = cmp.mapping.confirm({select = false}),
---         ["<Tab>"] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 cmp.select_next_item()
---             elseif luasnip.expand_or_jumpable() then
---                 luasnip.expand_or_jump()
---             elseif has_words_before() then
---                 cmp.complete()
---             else
---                 fallback()
---             end
---         end, {"i", "s"}),
---         ["<S-Tab>"] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 cmp.select_prev_item()
---             elseif luasnip.jumpable(-1) then
---                 luasnip.jump(-1)
---             else
---                 fallback()
---             end
---         end, {"i", "s"})
---     }),
---     sources = cmp.config.sources({{name = "nvim_lsp"}, {name = "luasnip"}},
---                                  {{name = "buffer"}, {name = "calc"}}),
---     completion = {keyword_length = 3}
--- })
-
--- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline("/", {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = {{name = "buffer"}}
--- })
-
--- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(":", {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
 -- })
