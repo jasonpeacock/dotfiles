b5ece7f58c42b9687271d6efd522cf6e07efa712
Jason Peacock <jpeacock@amazon.com>
2024-01-20 19:26:03 +0000

    add Brazil default platform

--- .config/host-init/amazon.fish
+++ .config/host-init/amazon.fish
@@ -5,6 +5,7 @@ fish_add_path $HOME/.toolbox/bin
 set -gx APPEND_CR_URL 1 # auto-add CR links to git commits
 set -gx BRAZIL_COLORS 1 # show colors in the output
 set -gx BRAZIL_WORKSPACE_DEFAULT_LAYOUT short # Use short workspace layout in Brazil
+set -gx BRAZIL_PLATFORM_OVERRIDE=AL2_x86_64 # Force default to AL2
 set -gx MAKE_OUTPUT_LEVEL QUIET # reduce noise during builds
 
 # Kuiper
@@ -15,5 +16,6 @@ set -gx DEV_ACCOUNT 594035389188 # for CDK builds
 # Aliases
 alias bb "SKIP_DOCUMENTATION=1 brazil-build"
 alias bre brazil-runtime-exec
+alias bte brazil-test-exec
 alias bbte brazil-build-tool-exec
 alias ki '/usr/bin/kinit -f'
