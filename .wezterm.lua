61c8180d9f3ac6938a92dfef5cf590791a01d1d1
Jason Peacock <jpeacock@amazon.com>
2024-10-28 08:35:37 -0700

    align wez color theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.default_prog = { "/Users/jpeacock/.nix-profile/bin/fish", "--login" }
 
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = "Gruvbox dark, soft (base16)"
 config.font = wezterm.font("GoMono Nerd Font Mono")
 -- config.font = wezterm.font("IosevkaTermSlab Nerd Font Mono", { weight = "Medium" })
 config.font_size = 13.0
